ab592d7ba0bbec152d6774b3f46e2345d0bd4215
misalgos <99750691+misalgos@users.noreply.github.com>
2022-02-16 20:04:33 -0500

    297#serialize-and-deserialize-binary-tree#Hard#132 ms#81.83%#54 MB#20.91%#00:30:00

---
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.js
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function(root) {
+    const res = [];
+    function dfs(node) {
+        if(!node) {
+            res.push('null');
+            return;
+        }
+        res.push(String(node.val));
+        dfs(node.left);
+        dfs(node.right);
+    }
+    dfs(root);
+    return res;
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function(data) {
+    let i = 0;
+    function dfs() {
+        if(data[i]==='null') {
+            i++;
+            return null;
+        }
+        const node = new TreeNode(Number(data[i]));
+        i++;
+        node.left = dfs();
+        node.right = dfs();
+        return node;
+    }
+    return dfs();
+}
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
