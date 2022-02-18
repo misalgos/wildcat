57491812db6954ec94c165321b783cc4ab4806d2
misalgos <99750691+misalgos@users.noreply.github.com>
2022-02-17 23:35:05 -0500

    882#reachable-nodes-in-subdivided-graph#Hard#268 ms#87.50%#62.8 MB#93.75%#00:30:00

---
+++ 882-reachable-nodes-in-subdivided-graph/882-reachable-nodes-in-subdivided-graph.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[][]} edges
+ * @param {number} maxMoves
+ * @param {number} n
+ * @return {number}
+ */
+const initializeGraph = (n) => { let G = []; for (let i = 0; i < n; i++) { G.push([]); } return G; };
+const addEdgeWithCostToG = (G, Edges) => { for (const [u, v, cost] of Edges) { G[u].push([v, cost]); G[v].push([u, cost]); } };
+
+const reachableNodes = (edges, maxMoves, n) => {
+    let g = initializeGraph(n);
+    addEdgeWithCostToG(g, edges);
+    return dijkstra(g, n, edges, maxMoves);
+};
+
+const MIN = Number.MIN_SAFE_INTEGER;
+const dijkstra = (g, n, edges, maxMoves) => {
+    let pq = new MaxPriorityQueue({ priority: x => x[0] });
+    let blood = Array(n).fill(MIN); // for each node, save maximum moves left to reach each node
+    pq.enqueue([maxMoves, 0]);
+    let res = 0;
+    while (pq.size()) {
+        let [hp, cur] = pq.dequeue().element;
+        if (blood[cur] != MIN) continue;
+        blood[cur] = hp;
+        res++;
+        for (const [next_node, cost] of g[cur]) {
+            let next_hp = hp - cost - 1; // cut the cost for move to adjacent child
+            if (next_hp < 0 || blood[next_node] != MIN) continue;
+            pq.enqueue([next_hp, next_node]);
+        }
+    }
+    for (const [u, v, cost] of edges) {
+        let uv = blood[u] == MIN ? 0 : blood[u];
+        let vu = blood[v] == MIN ? 0 : blood[v];
+        res += Math.min(cost, uv + vu);
+    }
+    return res;
+};
