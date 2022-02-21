0f7072f190b55910682dcedd548c6b7252b6e802
misalgos <99750691+misalgos@users.noreply.github.com>
2022-02-21 07:20:15 -0500

    Time: 72 ms, Memory: 49 MB

---
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,29 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+    if(!root) return root;
+    const queue = [root];
+    while(queue.length) {
+        let len = queue.length; 
+        for(let i = 0; i < len; i++) {
+            const node = queue.shift();
+            //If we there is at least one node left ahead of the current one
+            if(i < len-1) node.next = queue[0];
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+    }
+    return root;
+};
