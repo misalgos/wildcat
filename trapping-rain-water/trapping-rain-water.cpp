882c4062ee3f6801e90bda1ca58cd8995e3dda7c
misalgos <99750691+misalgos@users.noreply.github.com>
2022-02-22 21:42:51 -0500

    Time: 12 ms, Memory: 18.1 MB

--- trapping-rain-water/trapping-rain-water.cpp
+++ trapping-rain-water/trapping-rain-water.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int trap(vector<int>& height)  {
-    int ans = 0, current = 0;
-    stack<int> st;
-    while (current < height.size()) {
-        while (!st.empty() && height[current] > height[st.top()]) {
-            int top = st.top();
-            st.pop();
-            if (st.empty()) break;
-            int distance = current - st.top() - 1;
-            int bounded_height = min(height[current], height[st.top()]) - height[top];
-            ans += distance * bounded_height;
+  int trap(vector<int>& height) {
+        int ans = 0, current = 0;
+        stack<int> st;
+        while(current < height.size()) {
+            while(!st.empty() && height[current] > height[st.top()]) {
+                int top = st.top();
+                st.pop();
+                if(st.empty()) break;
+                int distance = current - st.top() - 1;
+                int bh = min(height[current], height[st.top()]) - height[top];
+                ans += distance * bh; 
+            }
+            st.push(current++);
         }
-        st.push(current++);
+        return ans;
     }
-    return ans;
-    }  
 };
