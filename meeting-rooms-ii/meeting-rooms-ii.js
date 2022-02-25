2c3543bf1572727e1b0b1a3911d88580ce8f1528
misalgos <99750691+misalgos@users.noreply.github.com>
2022-02-24 20:38:46 -0500

    Time: 92 ms, Memory: 44.5 MB

---
+++ meeting-rooms-ii/meeting-rooms-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var minMeetingRooms = function(intervals) {
+    const starts = intervals.map(m=> m[0]).sort((a,b)=> a - b);
+    const ends = intervals.map(m=> m[1]).sort((a,b)=> a - b);
+    let rooms = intervals.length;
+    let i = 0, j = 0;
+    while(i <starts.length && j < ends.length) {
+        if(starts[i] < ends[j]) {
+            i++;
+        } else {
+            i++;
+            j++;
+            rooms--;
+        }
+    }
+    return rooms;
+};
