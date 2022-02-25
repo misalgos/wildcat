217b49b23f12b698b53c56d668da209111c4dc62
misalgos <99750691+misalgos@users.noreply.github.com>
2022-02-24 20:02:10 -0500

    Time: 585 ms, Memory: 105.7 MB

--- sliding-window-maximum/sliding-window-maximum.js
+++ sliding-window-maximum/sliding-window-maximum.js
@@ -4,17 +4,12 @@
  * @return {number[]}
  */
 var maxSlidingWindow = function(nums, k) {
-    const ans = [];
     const heap = new MaxPriorityQueue();
+    const res = [];
     for(let i = 0; i < nums.length; i++) {
-        /*Remove the elements worth removing that are in the front; those that are the highest, but are also               located in indices before the current window (they would impact our evaluation of what the current               highest is.*/
-        while(heap.size() && heap.front().element <= i-k) {
-            heap.dequeue();
-        } 
-        heap.enqueue(i, nums[i]) // Enqueue the index and the current number 
-        if(i >= k-1) {
-            ans.push(heap.front().priority);
-        }
+        while(heap.size() && heap.front().element <= i-k) heap.dequeue();
+        heap.enqueue(i, nums[i]);
+        if(i >= k-1) res.push(heap.front().priority);
     }
-    return ans;
+    return res;
 };
