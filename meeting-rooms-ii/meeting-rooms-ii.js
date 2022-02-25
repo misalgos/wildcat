333427116b16dcaa792dfb06c79195ea81fdf6a6
misalgos <99750691+misalgos@users.noreply.github.com>
2022-02-24 20:44:40 -0500

    Time: 76 ms, Memory: 44.7 MB

--- meeting-rooms-ii/meeting-rooms-ii.js
+++ meeting-rooms-ii/meeting-rooms-ii.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
-    const starts = intervals.map(m=> m[0]).sort((a,b)=> a - b);
-    const ends = intervals.map(m=> m[1]).sort((a,b)=> a - b);
+    const starts = intervals.map(m => m[0]).sort((a,b)=> a - b);
+    const ends = intervals.map(m => m[1]).sort((a,b)=> a - b);
     let rooms = intervals.length;
     let i = 0, j = 0;
-    while(i <starts.length && j < ends.length) {
+    while(i < starts.length && j < ends.length) {
         if(starts[i] < ends[j]) {
             i++;
         } else {
