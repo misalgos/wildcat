519d6299a00dade596247d1bbd82c58bf98561bd
misalgos <99750691+misalgos@users.noreply.github.com>
2022-03-01 13:12:35 -0500

    Time: 585 ms, Memory: 106.3 MB

--- sliding-window-maximum/sliding-window-maximum.js
+++ sliding-window-maximum/sliding-window-maximum.js
@@ -5,11 +5,19 @@
  */
 var maxSlidingWindow = function(nums, k) {
     const heap = new MaxPriorityQueue();
-    const res = [];
+    const result = [];
     for(let i = 0; i < nums.length; i++) {
-        while(heap.size() && heap.front().element <= i-k) heap.dequeue();
+        while(!heap.isEmpty() && heap.front().element <= i - k) {
+            //Remove max values that aren't within the current window (i-k+1).
+            heap.dequeue();
+        }
+        //Enqueue the current number and index into the heap which will heap sort it in O(log(n)) time.
+        //Each heap node will contain both the index and number, but will be sorted by number descendingly.
         heap.enqueue(i, nums[i]);
-        if(i >= k-1) res.push(heap.front().priority);
+        //Whenever 'i' becomes greater than or equal to k-1, it means that a window of size k has been visited.
+        //Every subsequent iteration will have at least k-1 elements before it, completing the window size.
+        //And for every window of size k, push the number at the front of the heap, which is the max number.
+        if(i >= k - 1) result.push(heap.front().priority);
     }
-    return res;
+    return result;
 };
