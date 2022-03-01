968e250699786b95b3b95b2db9d924fb28466c92
misalgos <99750691+misalgos@users.noreply.github.com>
2022-02-28 20:41:08 -0500

    Time: 105 ms, Memory: 44.7 MB

--- trapping-rain-water/trapping-rain-water.js
+++ trapping-rain-water/trapping-rain-water.js
@@ -4,14 +4,14 @@
  */
 var trap = function(height) {
     const stack = [];
-    let area = 0;
+    let area = 0; 
     for(let i = 0; i < height.length; i++) {
         while(stack.length && height[stack[stack.length-1]] < height[i]) {
             const wallIdx = stack.pop();
             if(!stack.length) break;
             const currWidth = i - stack[stack.length-1] - 1;
             const currHeight = Math.min(height[stack[stack.length-1]], height[i]) - height[wallIdx];
-            area += currHeight * currWidth;
+            area += currWidth * currHeight;
         }
         stack.push(i);
     }
