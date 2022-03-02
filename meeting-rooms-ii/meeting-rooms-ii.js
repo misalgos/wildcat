9f52a5d9b05e5c7d21d251aa2bf2aab3d247b669
misalgos <99750691+misalgos@users.noreply.github.com>
2022-03-01 20:24:02 -0500

    Time: 78 ms, Memory: 44.6 MB

--- meeting-rooms-ii/meeting-rooms-ii.js
+++ meeting-rooms-ii/meeting-rooms-ii.js
@@ -3,42 +3,28 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
-    let rooms = 0;
     intervals = intervals.sort((a,b)=> a[0] - b[0]);
-    for(let i = 0; i < intervals.length; i++) {
-    /*For all of the meetings, we want to figure out if the ones that started before the current have
-    ended before the current one started. To verify this, we will compare the end times of meeting 'j' 
-    from the end times of the first meeting until the current meeting 'i' 
-    i.e. (0-'i' where meeting[j][1] <= meeting[i][0]). A flag boolean variable keeping track of this
-    so that when a meeting 'j' ended before the start of meeting 'i', we set the flag to true and break.
-    After the break, if the condition was NOT met, we increment the room count by one.
-    The reason why the search for meetings 'j' that ended before the start of meeting 'i' ends here is 
-    because we are concerned with finding a meeting that ended before or when the current one starts so
-    that we can determine whether we need another room or not and increment the room count if we do need
-    one. 
-    
-    Intuitively, this algorithm works similarly to the way a human would keep track of which meetings
-    are currently in session and which ones have ended to determine if a previously used room is freed 
-    up, or if we have to use an entirely new room altogether. The determinations in real time, in the 
-    that every meeting will commence. For every meeting that's about to start, one would check if any of
-    the rooms that were being used are freed up so that the meeting can take place there instead of having 
-    to get an additional room thus adding to the total amount of rooms that have to be used.
-    
-    Scheduling algorithms are  useful for logistics, operations, and even cpu resource allocation.  
-
-    For more information see the links related to the following one: 
-    https://en.wikipedia.org/wiki/Interval_scheduling
-    */
-        let isFreed = false;
-        for(let j = 0; j < i; j++) {
-            if(intervals[j][0] !== -1 && intervals[j][1] !== -1 && intervals[j][1]  <= intervals[i][0]) {
-                intervals[j][1] = -1;
-                intervals[j][0] = -1; 
-                isFreed = true;
-                break;
+    let size = intervals.length;
+    let rooms = 0;
+    let k = 0;
+    while(true) {
+        k = 0;
+        //If the kth interval has already been processed, move to the next one.
+        while(k < size && intervals[k][0] === -Infinity) k++;
+        //Be sure to break out if k has reached the end.        
+        if(k === size) break;
+        intervals[k][0] = -Infinity;
+        let currEnd = intervals[k][1];
+        for(let i = k; i < size; i++) {
+            /*Skip over any visited intervals*/
+            if(intervals[i][0] === -Infinity) continue;
+            /*If the current interval's end is before the 'i'th interval*/
+            if(currEnd <= intervals[i][0]) {
+                intervals[i][0] = -Infinity;
+                currEnd = intervals[i][1];
             }
         }
-        if(!isFreed) rooms++; //If none of the previously  booked rooms were freed increase the room count.
-    }
+        rooms++;
+    }  
     return rooms;
-}; 
+};
