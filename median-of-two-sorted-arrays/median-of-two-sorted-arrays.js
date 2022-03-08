757448f405be1dc2d93771614b4ef0bfe8d25f72
misalgos <99750691+misalgos@users.noreply.github.com>
2022-03-08 14:07:10 -0500

    Time: 175 ms, Memory: 46.5 MB

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -3,30 +3,30 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function(nums1, nums2) {
-    if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
-    let left = 0, right = nums1.length;
-    while(left <= right) {
-        let mid1 = Math.floor((left+right)/2);
-        let mid2 = Math.floor((nums1.length + nums2.length + 1) / 2) - mid1;
-        //Boundaries
-        let nums1Left = mid1 == 0 ? -Infinity : nums1[mid1 - 1];
-        let nums1Right = mid1 == nums1.length ? Infinity : nums1[mid1];
-        let nums2Left = mid2 == 0 ? -Infinity : nums2[mid2 - 1];
-        let nums2Right = mid2 == nums2.length ? Infinity : nums2[mid2];
-        if(nums1Left <= nums2Right && nums2Left <= nums1Right) { 
-            if((nums1.length + nums2.length) % 2 === 0) {
-                return (Math.max(nums1Left, nums2Left) + Math.min(nums1Right, nums2Right))/2;
+var findMedianSortedArrays = function(numsA, numsB) {
+    if (numsA.length > numsB.length) {
+        return findMedianSortedArrays(numsB, numsA);
+    }
+    let left = 0, right = numsA.length;
+    while (left <= right) {
+        const midA = Math.floor((left + right) / 2);
+        const midB = Math.floor((numsA.length + numsB.length + 1) / 2) - midA;
+        //Boundaries:
+        const leftA = midA === 0 ? -Infinity : numsA[midA-1];
+        const rightA = midA === numsA.length ? Infinity : numsA[midA];
+        const leftB = midB === 0 ? -Infinity : numsB[midB-1];
+        const rightB = midB === numsB.length ? Infinity : numsB[midB];
+        if (leftA <= rightB && leftB <= rightA) {
+            if ((numsA.length + numsB.length) % 2 === 0) {
+                return (Math.max(leftA, leftB) + Math.min(rightA, rightB)) / 2;
             } else {
-            //
-                return Math.max(nums1Left, nums2Left);
+                return Math.max(leftA, leftB);
             }
         }
-        
-        if(nums1Left > nums2Right) {
-            right = mid1 - 1;
+        if (leftA > rightB) {
+            right = midA - 1; 
         } else {
-            left = mid1 + 1;
+            left = midA + 1;
         }
     }
 };
