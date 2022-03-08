ed21f52e2b5edce859bc68903e771c5a69f979fe
misalgos <99750691+misalgos@users.noreply.github.com>
2022-03-08 18:27:33 -0500

    33#search-in-rotated-sorted-array#Medium#68 ms#84.21%#42.7 MB#8.46%#00:20:00

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -4,41 +4,27 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    const n = nums.length; 
-    if(n === 0) {
+    const n = nums.length;
+    if (n === 0) {
         return -1;
     }
-    let left = 0;
-    let right = n - 1;
-    let first = nums[0];
+    let left = 0, right = n-1, first = nums[0];
     while (left <= right) {
-        let mid = Math.floor(left + (right - left) / 2);
-        let value = nums[mid];
-        if(value === target) {
+        const mid = Math.floor(left + (right - left) /2 );
+        const val = nums[mid];
+        if (val === target) {
             return mid;
         }
-    /*Have a boolean that determines whether the current middle element is greater than the 
-    first element, which is useful to know to determine if we are at an index before the reset 
-    would happen in the shifting of the array.
-    Have another boolean to denoting if the target is greater than the first element so that 
-    we can use it to determine if the target is not at an index at or after which the reset happened.
-    */
-        let valueAsc = value >= first;
-        let targetAsc = target >= first;
-        if (valueAsc === targetAsc) {
-    /*If both the value and the target are in the same 'increasing-from-start-part', do a standard binary search
-    boundary update... If the value is less than the target, increment the left otherwise, decrement the right.*/
-            if (value < target) {
+        const valAsc = val >= first;
+        const targetAsc = target >= first;
+        if (valAsc === targetAsc) {
+            if (val < target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         } else {
-    /*Otherwise, either the value is in the 'increasing-from-start-part' or the target is, but not both.
-    Check which one is in this ascending part so that we can determine which boundary to update. If the
-    value is the one in the ascending portion, then we must search to the right of our window, where the reset  index will eventually be encountered to find the target value, so update the left side to be the index after the middle. 
-    If the target is in the ascending portion of the array, it means that the search needs to be moved left of the current window to find this target value. Update the right to be the index before the midpoint.*/
-            if (valueAsc) {
+            if (valAsc) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
