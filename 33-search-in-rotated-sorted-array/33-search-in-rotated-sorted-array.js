b234d647b2778928ea3448a6fbfea15476f29d6a
misalgos <99750691+misalgos@users.noreply.github.com>
2022-03-08 18:32:15 -0500

    33#search-in-rotated-sorted-array#Medium#58 ms#95.90%#42.4 MB#28.25%#00:20:00

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -9,22 +9,22 @@ var search = function(nums, target) {
         return -1;
     }
     let left = 0, right = n-1, first = nums[0];
-    while (left <= right) {
-        const mid = Math.floor(left + (right - left) /2 );
+    while(left <= right) {
+        const mid = Math.floor(left + (right-left) /2);
         const val = nums[mid];
         if (val === target) {
             return mid;
         }
         const valAsc = val >= first;
         const targetAsc = target >= first;
-        if (valAsc === targetAsc) {
-            if (val < target) {
+        if (valAsc ^ targetAsc) {
+            if (valAsc) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         } else {
-            if (valAsc) {
+            if (val < target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
