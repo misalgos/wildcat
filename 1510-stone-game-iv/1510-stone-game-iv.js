31c683af23029bf37991d15fa8167d6c8392cc2b
misalgos <99750691+misalgos@users.noreply.github.com>
2022-03-09 22:55:45 -0500

    1510#stone-game-iv#Hard#103 ms#91.70%#44.7 MB#52.45%#00:30:00

---
+++ 1510-stone-game-iv/1510-stone-game-iv.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var winnerSquareGame = function(n) {
+    const dp = new Array(n+1).fill(false);
+    for (let i = 0; i < n+1; i++) {
+        for (let k = 1; k * k <=i; k++) {
+            if (dp[i - k * k] === false) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+    return dp[n];
+};
