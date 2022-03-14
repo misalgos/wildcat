655c22d537db8244e7e31dfa75364f831c77f68d
misalgos <99750691+misalgos@users.noreply.github.com>
2022-03-13 21:59:07 -0400

    828#count-unique-characters-of-all-substrings-of-a-given-string#Hard#283 ms#9.95%#45.6 MB#71.56%#00:30:00

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.js
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.js
@@ -15,19 +15,20 @@ var uniqueLetterString = function(s) {
             newContr = the index of the current letter minus the last position in which it was last seen plus one. 
             (this is similar to the 'end - start + 1', because it is a measurement of the window size of each letter's occurrence),
             
-            currUniques = 
-            
-            
-            
+            currUniques = however many current uniques there are minus the last window size plus the size of
+            the new window       
         */
         const prevContr = contr.has(c) ? contr.get(c) : 0;
         const newContr = i - (lastPos.has(c) ? lastPos.get(c) + 1 : 0) + 1;
-        
         currUniques = currUniques - prevContr + newContr;
         
+        /*
+            Save the current window in the map.
+            And save the position of the current letter in a map.
+            Add the  current unique count to the result.
+        */
         contr.set(c, newContr);
         lastPos.set(c, i);
-        
         result += currUniques;
     }
     return result;
