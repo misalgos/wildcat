89441cd2423750356cd5edb253fb687dafbd9e67
misalgos <99750691+misalgos@users.noreply.github.com>
2022-03-14 12:48:15 -0400

    828#count-unique-characters-of-all-substrings-of-a-given-string#Hard#213 ms#24.17%#45.6 MB#68.72%#00:30:00

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.js
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.js
@@ -3,33 +3,20 @@
  * @return {number}
  */
 var uniqueLetterString = function(s) {
-    let currUniques = 0;
-    let lastPos = new Map(); //last seen position
-    let contr = new Map();
+    let uniques = 0;
     let result = 0;
-    for (let i = 0; i < s.length; i++) {
-        const c = s[i];
-        /*
-            prevContr = the last window size between the previous two occurences of the current letter.
-            
-            newContr = the index of the current letter minus the last position in which it was last seen plus one. 
-            (this is similar to the 'end - start + 1', because it is a measurement of the window size of each letter's occurrence),
-            
-            currUniques = however many current uniques there are minus the last window size plus the size of
-            the new window       
-        */
-        const prevContr = contr.has(c) ? contr.get(c) : 0;
-        const newContr = i - (lastPos.has(c) ? lastPos.get(c) + 1 : 0) + 1;
-        currUniques = currUniques - prevContr + newContr;
+    const startsAt = new Map();
+    const windows = new Map();
+    for (let end = 0; end < s.length; end++) {
+        const c = s[end];
+        const prevWindow = windows.has(c) ? windows.get(c) : 0;
+        const start = (startsAt.has(c) ? startsAt.get(c) + 1 : 0);
+        const currWindow = end - start + 1;
         
-        /*
-            Save the current window in the map.
-            And save the position of the current letter in a map.
-            Add the  current unique count to the result.
-        */
-        contr.set(c, newContr);
-        lastPos.set(c, i);
-        result += currUniques;
+        uniques = uniques - prevWindow + currWindow;
+        windows.set(c, currWindow);
+        startsAt.set(c, end);
+        result += uniques;
     }
     return result;
 };
