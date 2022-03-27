b3f6b608335b7c64cf152f363d3a2c2ca6260521
misalgos <99750691+misalgos@users.noreply.github.com>
2022-03-26 23:40:15 -0400

    173#binary-search-tree-iterator#Medium#151 ms#74.68%#55.3 MB#78.97%#00:20:00

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.js
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.js
@@ -1,43 +1,23 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- */
 class BSTIterator {
-        constructor(root) {
+    constructor(root) {
         this.root = root;
         this.ptr = this.root;
         this.stack = [];
-        this.inorder();
+        this._inorder();
     }
-    inorder() {
-        if (this.ptr || this.stack.length) {
-            while (this.ptr) {
-                this.stack.push(this.ptr);
-                this.ptr = this.ptr.left;
-            }
+    _inorder() {
+        while (this.ptr) {
+            this.stack.push(this.ptr);
+            this.ptr = this.ptr.left;
         }
     }
     hasNext() {
         return this.stack.length;
     }
     next() {
-        const top = this.stack.pop();
-        this.ptr = top.right;
-        this.inorder();
-        return top.val;
+        const nextNode = this.stack.pop();
+        this.ptr = nextNode.right;
+        this._inorder();
+        return nextNode.val;
     }
 }
-
-/** 
- * Your BSTIterator object will be instantiated and called as such:
- * var obj = new BSTIterator(root)
- * var param_1 = obj.next()
- * var param_2 = obj.hasNext()
- */
